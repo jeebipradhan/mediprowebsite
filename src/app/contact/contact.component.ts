dc9f7f8a2b53f73a185e632a4c0bf0eb0c21d789
Gyanendra Pradhan <jeebipradhan@yahoo.com>
2025-06-03 17:45:38 -0600

    responsive page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,59 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
-interface RegionalContact {
-  name: string;
-  region: string;
-  phones: string[];
-  email: string;
+interface Contact {
+  RegionName: string;
+  Address: string | null;
+  Phone1: string;
+  Phone2?: string;
+  Email: string;
+  ContactPerson?: string;
+}
+
+interface ContactData {
+  contacts: Contact[];
 }
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
-  headOffice = {
-    title: 'Head Office',
-    location: 'Kathmandu Head Office',
-    address: 'Komal Complex, Putalisadak, 1588 Ramshah Path, Kathmandu.',
-    phone: '01-5970-396',
-    email: 'medipro@mediprocomputers.com'
-  };
+export class ContactComponent implements OnInit {
+  contacts: Contact[] = [];
+  loading = true;
+  error: string | null = null;
 
-  regionalContacts: RegionalContact[] = [
-    {
-      name: 'Kishan Pandey',
-      region: 'Western, Mid-Western, Far-Western',
-      phones: ['980-202-0240', '985-802-0240'],
-      email: 'hrishavpandey@gmail.com'
-    },
-    {
-      name: 'Kumar Khatiwada',
-      region: 'Eastern',
-      phones: ['980-202-0148', '985-202-0148'],
-      email: 'kumar.medipro@gmail.com'
-    },
-    {
-      name: 'Ashish Gurung',
-      region: 'Gandaki Province',
-      phones: ['980-202-0194', '985-602-0194'],
-      email: 'hangdudh@gmail.com'
-    },
-    {
-      name: 'Shekhar Dulal',
-      region: 'KAVRE',
-      phones: ['985-108-1595', '980-205-0888'],
-      email: 'shekhardulal@gmail.com'
-    },
-    {
-      name: 'Anil Shrestha',
-      region: 'Narayangarh',
-      phones: ['985-506-0164'],
-      email: 'sthanil.medipro@gmail.com'
-    }
-  ];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadContacts();
+  }
+
+  private loadContacts() {
+    this.http.get<ContactData>('assets/contact.json').subscribe({
+      next: (data) => {
+        this.contacts = data.contacts;
+        this.loading = false;
+      },
+      error: (error) => {
+        this.error = 'Failed to load contact information';
+        this.loading = false;
+        console.error('Error loading contacts:', error);
+      }
+    });
+  }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'] // optional
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
