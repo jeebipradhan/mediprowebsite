2a677606541d2ff033e1e77aba5b0fa3bc462fdb
Gyanendra Pradhan <jeebipradhan@yahoo.com>
2025-06-19 13:09:37 -0600

    Solutions & Resources pages added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { TeamComponent } from './team/team.component';
 import { EventComponent } from './event/event.component';
 import { ContactComponent } from './contact/contact.component';
 import { DownloadsComponent } from './downloads/downloads.component';
+import { SolutionsComponent } from './solutions/solutions.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -17,5 +19,7 @@ export const routes: Routes = [
   { path: 'event', component: EventComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'downloads', component: DownloadsComponent },
+  { path: 'solutions', component: SolutionsComponent },
+  { path: 'resources', component: ResourcesComponent },
   { path: '**', redirectTo: '' } // Redirect any unknown routes to home
 ];
